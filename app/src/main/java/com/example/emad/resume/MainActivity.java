9e2b0d247e2ad43975f7fc2b699de8814cf0ee04
emad21 <mohd.emad@y7mail.com>
2016-03-14 16:45:55 -0700

    The function to load json from its file

--- app/src/main/java/com/example/emad/resume/MainActivity.java
+++ app/src/main/java/com/example/emad/resume/MainActivity.java
@@ -31,6 +31,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // this function will load json from its local json file
+      public String loadJSONFromAsset() {
+        String json = null;
+        try {
+            InputStream is = getAssets().open("resume.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
       
   @Override
     public boolean onCreateOptionsMenu(Menu menu) {
